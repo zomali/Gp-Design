1fef972614d7c252723505b49ae9d40b9d91bd5d
hoso99 <hosoabdelnaby@gmail.com>
2022-03-20 22:32:50 -0800

    new start // merge adham &ersaa&hazem

---
+++ lib/pdf_view.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+
+class pdf_view extends StatefulWidget {
+
+  @override
+  _pdf_view createState() => _pdf_view();
+}
+
+class _pdf_view extends State<pdf_view> {
+  late PdfViewerController _pdfViewerController;
+  final GlobalKey<SfPdfViewerState> _pdfViewerStateKey = GlobalKey();
+  @override
+  void initState() {
+    _pdfViewerController = PdfViewerController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: SfPdfViewer.network(
+              'https://firebasestorage.googleapis.com/v0/b/graduation-project-a9cdf.appspot.com/o/Revision%2C%20Variables%20%26%20Constants%2FEnglish%2FRevision%2C%20Variables%20%26%20Constants.pdf?alt=media&token=cb4470fe-1b65-48fb-8924-e494e7b2515c',
+              controller: _pdfViewerController,
+              key: _pdfViewerStateKey),
+          appBar: AppBar(
+            title: Text('Revision, Variables & Constants'),
+            actions: <Widget>[
+              IconButton(
+                  onPressed: () {
+                    _pdfViewerStateKey.currentState!.openBookmarkView();
+                  },
+                  icon: Icon(
+                    Icons.bookmark,
+                    color: Colors.white,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    //   _pdfViewerController.jumpToPage(5);
+                    _pdfViewerController.lastPage();
+
+                  },
+                  icon: Icon(
+                    Icons.arrow_drop_down_rounded,
+                    color: Colors.white,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    //   _pdfViewerController.jumpToPage(5);
+                    _pdfViewerController.firstPage();
+                  },
+                  icon: Icon(
+                    Icons.arrow_drop_up_rounded,
+                    color: Colors.white,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    _pdfViewerController.zoomLevel = 3.25;
+                  },
+                  icon: Icon(
+                    Icons.zoom_in,
+                    color: Colors.white,
+                  ))
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/topic_images.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+class image_view extends StatefulWidget {
+  @override
+  _image_view createState() => _image_view();
+}
+class _image_view extends State<image_view> {
+  var titleList = [
+    "Variable in C++",
+    "Variable in C++",
+    "Variable in C++",
+    "Variable in C++",
+    "Variable in C++",
+    "Variable in C++",
+    "Variable in C++",
+  ];
+
+  var srcList = [
+    "geeksforgeeks",
+    "geeksforgeeks",
+    "geeksforgeeks",
+    "geeksforgeeks",
+    "geeksforgeeks",
+    "geeksforgeeks",
+    "geeksforgeeks",
+  ];
+
+  var imgList = [
+    "assets/images/img1.jpg",
+    "assets/images/img2.jpg",
+    "assets/images/img3.jpg",
+    "assets/images/img4.jpg",
+    "assets/images/img5.jpg",
+    "assets/images/img6.jpg",
+    "assets/images/img7.jpg"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // MediaQuery to get Device Width
+    double width = MediaQuery.of(context).size.width * 0.6;
+    return Scaffold(
+      appBar: AppBar(
+        // App Bar
+        title: Text(
+          "Images Of Topic",
+          style: TextStyle(color: Colors.blue),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      // Main List View With Builder
+      body: ListView.builder(
+        itemCount: imgList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              // This Will Call When User Click On ListView Item
+              showDialogFunc(context, imgList[index], titleList[index], srcList[index]);
+            },
+            // Card Which Holds Layout Of ListView Item
+            child: Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 110,
+                    height: 110,
+                    child:Image.network('https://firebasestorage.googleapis.com/v0/b/graduation-project-a9cdf.appspot.com/o/Revision%2C%20Variables%20%26%20Constants%2FEnglish%2F1.1.2.jpg?alt=media&token=d703e865-5f01-40ef-a493-9bb3bce9cf21')
+                  ),
+                  Spacer(),
+                 Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          titleList[index],
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: width,
+                          child: Text(
+                            srcList[index],
+                            maxLines: 3,
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    ),
+                  Spacer(),
+
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// This is a block of Model Dialog
+showDialogFunc(context, img, title, desc) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return Center(
+        child: Material(
+          type: MaterialType.transparency,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+            padding: EdgeInsets.all(5),
+            height: 620,
+            width: MediaQuery.of(context).size.width * 2.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.network('https://firebasestorage.googleapis.com/v0/b/graduation-project-a9cdf.appspot.com/o/Revision%2C%20Variables%20%26%20Constants%2FEnglish%2F1.1.2.jpg?alt=media&token=d703e865-5f01-40ef-a493-9bb3bce9cf21',
+                    width: 500,
+                    height: 500,),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  // width: 200,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      desc,
+                      maxLines: 3,
+                      style: TextStyle(fontSize: 15, color: Colors.blue),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
